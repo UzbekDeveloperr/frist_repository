5043fac64ab9c60b05cefaf812384c74c3d4eaf4
Xushbaxtov Nodirbek <98225055+XNglobal@users.noreply.github.com>
2022-02-25 20:49:27 +0500

    Add files via upload

---
+++ royxat-bilan-ishlash.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 19:36:53 2022
+
+@author: nodir
+"""
+
+juft_sonlar=list(range(120,1201,2))
+katta=max(juft_sonlar)
+kichik=min(juft_sonlar)
+ayirma=katta-kichik
+
+
+davlatlar=["Anglya","Ispanya","Italia","Germanya","Amerika","Korea","Iroq","Kanada","Fransiya","Malayziya"]
+taomlar=["osh","manti","sho'rva","somsa","shashlik"]
+nonushta=taomlar[:]
+nonushta.remove("shashlik")
+nonushta.remove("sho'rva")
+nonushta.append("sut")
+nonushta.append("tost non")
+nonushta[0]="qaymoq va non"
